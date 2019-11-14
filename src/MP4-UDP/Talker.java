e7562bb3a04d0bbcb48a21856c44dd1629994f70
isaacpitblado <isaacpitblado@tamu.edu>
2019-11-14 13:02:21 -0600

    moved files around

---
+++ src/MP4-UDP/Talker.java
@@ -0,0 +1,18 @@
+
+public class Talker {
+  public boolean splitMessage(string s) {
+    int cnt = 0;
+    int step = 10;
+
+    while (cnt < s.length()) {
+      cnt += step;
+
+      if (cnt < s.length()) {
+
+      }
+
+    }
+
+  }
+
+}
